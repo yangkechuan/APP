c7dea602cd7e88dc6ad195da06ab8a57121dfa2e
ykc <907148778@qq.com>
2016-01-12 21:27:45 +0800

    Activity活动周期学习

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,18 +1,99 @@
+/**
+ *  活动生命周期
+ *
+ * onCreate()             -----完成活动的初始化操作
+ *
+ * onStart()             ------在活动中由不可见变为可见
+ *
+ * onResume()            ------在活动准备好和用户交互的时候调用，此时的活动一定位于返回栈的栈顶，并处于运行状态
+ *
+ * onPause()             ------在系统准备启动或恢复另一个活动的时候调用
+ *
+ * onStop()              ------在活动被销毁前调用
+ *
+ * onRestart()           ------在活动由停止变为运行状态之前调用
+ */
+
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+
+        Button startNormalActivity = (Button) findViewById(R.id.start_normal_acticity);
+        Button startDialogActivity = (Button) findViewById(R.id.start_dialog_activity);
+        startNormalActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        startDialogActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,DialogActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestory");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onRestart");
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
