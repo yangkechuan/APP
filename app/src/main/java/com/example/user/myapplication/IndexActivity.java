a6b546fd821130e3477d52298eb828c79a176e33
ykc <907148778@qq.com>
2016-01-14 18:32:16 +0800

    ProgressBar进度条点击事件与style设置

--- app/src/main/java/com/example/user/myapplication/IndexActivity.java
+++ app/src/main/java/com/example/user/myapplication/IndexActivity.java
@@ -3,11 +3,14 @@ package com.example.user.myapplication;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 /**
@@ -18,6 +21,9 @@ public class IndexActivity extends Activity implements View.OnClickListener {
 
     private Button index_btn1;
     private EditText index_editText1;
+    private ImageView index_imageView1;
+    private ProgressBar index_progressBar1,index_progressBar2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,12 @@ public class IndexActivity extends Activity implements View.OnClickListener {
         index_btn1.setOnClickListener(this);
         index_editText1 = (EditText) findViewById(R.id.index_editText1);
         index_editText1.setOnClickListener(this);
+        index_imageView1 = (ImageView) findViewById(R.id.index_imageView1);
+        index_imageView1.setOnClickListener(this);
+        index_progressBar1 = (ProgressBar) findViewById(R.id.index_progressBar1);
+        index_progressBar1.setOnClickListener(this);
+        index_progressBar2 = (ProgressBar) findViewById(R.id.index_progressBar2);
+        index_progressBar2.setOnClickListener(this);
     }
 
 
@@ -38,6 +50,25 @@ public class IndexActivity extends Activity implements View.OnClickListener {
                 String inputText = index_editText1.getText().toString();
                 Toast.makeText(IndexActivity.this,inputText,Toast.LENGTH_SHORT).show();
                 break;
+
+            case R.id.index_imageView1://点击替换图片
+                index_imageView1.setImageResource(R.drawable.ic_launcher);
+                break;
+
+            case R.id.index_progressBar1://点击取消进度条
+                if (index_progressBar1.getVisibility() == View.GONE){
+                    index_progressBar1.setVisibility(View.VISIBLE);
+                }else {
+                    index_progressBar1.setVisibility(View.GONE);
+                }
+                break;
+
+            case R.id.index_progressBar2://点击进度条，增加数值
+                int progress = index_progressBar2.getProgress();
+                progress+=10;
+                index_progressBar2.setProgress(progress);
+                break;
+
             default:
                 break;
         }
