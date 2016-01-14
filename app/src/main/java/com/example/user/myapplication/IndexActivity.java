956b25c379d88f6270bc8478d2fd95d3793fdfad
ykc <907148778@qq.com>
2016-01-14 19:53:21 +0800

    AlertDialog对话框

--- app/src/main/java/com/example/user/myapplication/IndexActivity.java
+++ app/src/main/java/com/example/user/myapplication/IndexActivity.java
@@ -1,6 +1,8 @@
 package com.example.user.myapplication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -11,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -19,10 +22,11 @@ import android.widget.Toast;
 
 public class IndexActivity extends Activity implements View.OnClickListener {
 
-    private Button index_btn1;
+    private Button index_btn1,index_btn2;
     private EditText index_editText1;
     private ImageView index_imageView1;
     private ProgressBar index_progressBar1,index_progressBar2;
+    private TextView index_textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,8 @@ public class IndexActivity extends Activity implements View.OnClickListener {
 
         index_btn1 =(Button)findViewById(R.id.index_btn1);
         index_btn1.setOnClickListener(this);
+        index_btn2 = (Button) findViewById(R.id.index_btn2);
+        index_btn2.setOnClickListener(this);
         index_editText1 = (EditText) findViewById(R.id.index_editText1);
         index_editText1.setOnClickListener(this);
         index_imageView1 = (ImageView) findViewById(R.id.index_imageView1);
@@ -39,6 +45,8 @@ public class IndexActivity extends Activity implements View.OnClickListener {
         index_progressBar1.setOnClickListener(this);
         index_progressBar2 = (ProgressBar) findViewById(R.id.index_progressBar2);
         index_progressBar2.setOnClickListener(this);
+        index_textView1 = (TextView) findViewById(R.id.index_textView1);
+        index_textView1.setOnClickListener(this);
     }
 
 
@@ -67,6 +75,32 @@ public class IndexActivity extends Activity implements View.OnClickListener {
                 int progress = index_progressBar2.getProgress();
                 progress+=10;
                 index_progressBar2.setProgress(progress);
+                index_textView1.setText("当前进度："+index_progressBar2.getProgress()+"/100");
+                break;
+
+            /**
+             * 通过AlterDialog.Builder创建一个AlertDialog实例，设置标题，内容，可否取消等属性
+             * 设置setPositiveButton()和setNegativeButton(),分别为确定按钮和取消按钮的点击事件
+             * 最后show()方法将对话框显示
+             */
+            case R.id.index_btn2://触发一个AlertDialog事件
+                AlertDialog.Builder dialog = new AlertDialog.Builder(IndexActivity.this);
+                dialog.setTitle("this is Dialog");
+                dialog.setMessage("hello world");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(IndexActivity.this, "OK", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(IndexActivity.this, "Cancle", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.show();
                 break;
 
             default:
