c63620e10a0657c3911b7ce09819199a7cf4cb70
ykc <907148778@qq.com>
2016-01-19 20:49:47 +0800

    new png

--- app/src/main/java/com/example/user/myapplication/TitleLayout.java
+++ app/src/main/java/com/example/user/myapplication/TitleLayout.java
@@ -7,7 +7,7 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -27,8 +27,8 @@ public class TitleLayout extends LinearLayout {
          */
         LayoutInflater.from(context).inflate(R.layout.title,this);
 
-        Button titleBack = (Button) findViewById(R.id.title_back);
-        Button titleEdit = (Button) findViewById(R.id.title_edit);
+        ImageButton titleBack = (ImageButton) findViewById(R.id.title_back);
+        ImageButton titleEdit = (ImageButton) findViewById(R.id.title_edit);
         titleBack.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
